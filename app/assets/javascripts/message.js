cc16da36168fffde50d0aca9f1e0fff5408f9f2b
azunyan0720jp <azusa.goto@gmail.com>
2019-08-28 21:34:18 +0900

    chat-space-automatic
    #WHAT
    チャットスペースに自動更新機能を実装。
    #WHY
    チャットツールとして、自動更新されなければ即時にメッセージを送ることができないため。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,32 @@
-$(document).on('turbolinks:load', function(){
+$(function () {
+  
   function buildHTML(message) {
-    var content = message.content ? `${ message.content }` : "";
-    var image = message.image == null ? "" : `<img src= "${ message.image }" class="lower-message__image">`;
-    var html = `<div class="message">
-                  <div class=“upper-message">
-                    <div class="upper-message__user-name">
-                      ${message.user_name}
-                    </div>
-                    <div class="upper-message__date">
-                      ${message.date}
-                    </div>
-                  </div>
-                  <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${content}
-                    </p>
-                  ${image}
-                  </div>
-                </div>`
-  return html;
+
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
+    
+    var html = `<div class="message" data-message-id="${message.id}">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-meesage">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
+    return html;
   }
-  $('#new_message').on('submit', function(e){
-   e.preventDefault();
+  $('#new_message').on('submit', function (e) {
+    e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action') 
+
     $.ajax({
       url: url,
       type: "POST",
@@ -32,15 +35,39 @@ $(document).on('turbolinks:load', function(){
       processData: false,
       contentType: false
     })
-    .done(function(formData) {
-      var html = buildHTML(formData);
+    .done(function (data) {
+      var html = buildHTML(data);
       $('.messages').append(html);
-      $('#new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__submit').prop('disabled', false);
-      })
-    .fail(function(){
-      alert('error');
-      })
+      $('form')[0].reset();
     })
-  })
+    .fail(function () {
+      alert('Please Type a Message!');
+    })
+  })  
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+      var group_id = $(".group").data("group-id");
+
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function () {
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
